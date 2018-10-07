2cc8a18623af52fd854c79f60c86cbe3298d795c
Fiona Wilson <fionaw237@gmail.com>
2018-10-07 09:31:01 +0100

    Adds Dealer class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,14 +4,13 @@ public class Runner {
 
     public static void main(String[] args){
         Player player1 = new Player("Player 1");
-        Player player2 = new Player("Dealer");
-        Player player3 = new Player("Player 2");
+        Player player2 = new Player("Player 2");
+        Dealer dealer = new Dealer("Dealer");
         ArrayList<Player> players = new ArrayList<>();
         players.add(player1);
         players.add(player2);
-        players.add(player3);
         Deck deck = new Deck();
-        Game game = new Game(players, deck, player2);
+        Game game = new Game(players, deck, dealer);
 
         game.play();
     }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,7 +9,7 @@ public class GameTest {
 
     Player player1;
     Player player2;
-    Player player3;
+    Dealer dealer;
     Deck deck;
     Game game;
     ArrayList<Player> players;
@@ -19,13 +19,12 @@ public class GameTest {
     public void before(){
         player1 = new Player("Player 1");
         player2 = new Player("Player 2");
-        player3 = new Player("Player 3");
+        dealer = new Dealer("Dealer");
         players = new ArrayList<>();
         players.add(player1);
         players.add(player2);
-        players.add(player3);
         deck = new Deck();
-        game = new Game(players, deck, player2);
+        game = new Game(players, deck, dealer);
         allowedChoices = new ArrayList<>();
         allowedChoices.add("s");
         allowedChoices.add("t");
@@ -33,12 +32,12 @@ public class GameTest {
 
     @Test
     public void hasDealer(){
-        assertEquals(player2, game.getDealer());
+        assertEquals(dealer, game.getDealer());
     }
 
     @Test
     public void canGetNoOfPlayers(){
-        assertEquals(3, game.numberOfPlayers());
+        assertEquals(2, game.numberOfPlayers());
     }
 
     @Test
@@ -46,7 +45,7 @@ public class GameTest {
         game.initialDeal();
         assertEquals(2, player1.numberOfCards());
         assertEquals(2, player2.numberOfCards());
-        assertEquals(2, player3.numberOfCards());
+        assertEquals(2, dealer.numberOfCards());
         assertEquals(46, game.getDeck().numberOfCards());
     }
 
@@ -54,44 +53,38 @@ public class GameTest {
     public void canGetWinner(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.TWO));
         player1.receiveCard(new Card(Suit.HEARTS, Rank.NINE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.THREE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.JACK));
-        assertEquals(player2, game.getWinner(player1));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.THREE));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.JACK));
+        assertEquals(dealer, game.getWinner(player1));
     }
 
     @Test
     public void isDraw(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.THREE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
         player1.receiveCard(new Card(Suit.HEARTS, Rank.SIX));
-        player2.receiveCard(new Card(Suit.DIAMONDS, Rank.THREE));
+        dealer.receiveCard(new Card(Suit.DIAMONDS, Rank.THREE));
         assertEquals(true, game.isDraw(player1));
     }
 
     @Test
     public void isNotDraw(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
         player1.receiveCard(new Card(Suit.HEARTS, Rank.TWO));
-        player2.receiveCard(new Card(Suit.DIAMONDS, Rank.TWO));
+        dealer.receiveCard(new Card(Suit.DIAMONDS, Rank.TWO));
         assertEquals(false, game.isDraw(player1));
     }
 
     @Test
     public void getWinnerReturnsNullIfDraw(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.SIX));
         player1.receiveCard(new Card(Suit.HEARTS, Rank.SIX));
-        player2.receiveCard(new Card(Suit.DIAMONDS, Rank.ACE));
+        dealer.receiveCard(new Card(Suit.DIAMONDS, Rank.ACE));
         assertEquals(null, game.getWinner(player1));
     }
 
-    @Test
-    public void canChangeDealer(){
-        game.changeDealer(player1);
-        assertEquals(true, player1.checkIfDealer());
-        assertEquals(false, player2.checkIfDealer());
-    }
 
     @Test
     public void checkUserInputTrue(){
@@ -108,11 +101,9 @@ public class GameTest {
     @Test
     public void bothScore21DealerWinsWithBlackjack(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
         player1.receiveCard(new Card(Suit.HEARTS, Rank.TEN));
-        player2.receiveCard(new Card(Suit.DIAMONDS, Rank.KING));
-//        player1.chooseAceHigh();
-//        player2.chooseAceHigh();
+        dealer.receiveCard(new Card(Suit.DIAMONDS, Rank.KING));
         assertEquals(21, game.getDealer().getHandValue());
         assertEquals(21, player1.getHandValue());
         assertEquals(false, game.isDraw(player1));
@@ -123,11 +114,9 @@ public class GameTest {
     @Test
     public void bothScore21PlayerWinsWithBlackjack(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.HEARTS, Rank.TEN));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.receiveCard(new Card(Suit.HEARTS, Rank.TEN));
         player1.receiveCard(new Card(Suit.DIAMONDS, Rank.KING));
-//        player1.chooseAceHigh();
-//        player2.chooseAceHigh();
         assertEquals(21, game.getDealer().getHandValue());
         assertEquals(21, player1.getHandValue());
         assertEquals(false, game.isDraw(player1));
@@ -137,11 +126,9 @@ public class GameTest {
     @Test
     public void bothHaveBlackjackDraw(){
         player1.receiveCard(new Card(Suit.HEARTS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
-        player2.receiveCard(new Card(Suit.HEARTS, Rank.QUEEN));
+        dealer.receiveCard(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.receiveCard(new Card(Suit.HEARTS, Rank.QUEEN));
         player1.receiveCard(new Card(Suit.DIAMONDS, Rank.KING));
-//        player1.chooseAceHigh();
-//        player2.chooseAceHigh();
         assertEquals(21, game.getDealer().getHandValue());
         assertEquals(21, player1.getHandValue());
         assertEquals(true, game.isDraw(player1));
